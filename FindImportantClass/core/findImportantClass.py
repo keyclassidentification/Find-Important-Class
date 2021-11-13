49dc46d98a925d69c21b2bf125ba7f68c37c4dc3
penny tao <853725633@qq.com>
2021-11-13 23:17:49 +0800

    Add graphHelper.py & fileHelper.py & classAttention.py

--- FindImportantClass/core/findImportantClass.py
+++ FindImportantClass/core/findImportantClass.py
@@ -3,7 +3,9 @@ import numpy as np
 from class2vec import Node2Vec
 import networkx as nx
 import tensorflow as tf
-from core.dataHelper import copy_directed_graph, adj_to_bias,get_neighbours,read_node_label,save_node_label,save_rank
+from core.graphHelper import copy_directed_graph, adj_to_bias,get_neighbours
+from core.fileHelper import read_node_label,save_node_label,save_rank
+from core.classAttention import getNodeAttentions
 
 def loadData():
     G = nx.read_pajek('E:/实验/DATA/weight/ant.txt')
@@ -46,23 +48,6 @@ head = 50
 Graph,IdNumber,name, features, wei,bias= loadData()
 ft_size = features.shape[1]
 nb_nodes = features.shape[0]
-
-def getNodeAttentions(seq,weight_mat, bias_mat,out_sz, activation=tf.nn.elu):
-    with tf.name_scope('my_attn'):
-        #计算Wh得到seq_fts,变换后的特征矩阵
-        seq_fts = tf.compat.v1.layers.conv1d(seq, out_sz, 1, use_bias=False)
-
-        # simplest self-attention possible
-        #节点投影
-        f_1 = tf.compat.v1.layers.conv1d(seq_fts, 1, 1)
-        #节点邻居投影
-        f_2 = tf.compat.v1.layers.conv1d(seq_fts, 1, 1)
-        #将 f_2 转置之后与 f_1 叠加，得到注意力矩阵
-        logits = f_1 + tf.transpose(f_2, [0, 2, 1]) #+weight_mat
-        #注意力权重
-        coefs = tf.nn.softmax(tf.nn.leaky_relu(logits) + bias_mat)
-
-        return coefs[0]
 
 tf.compat.v1.disable_eager_execution()
 # temp add
--- FindImportantClass/core/graphHelper.py
+++ FindImportantClass/core/graphHelper.py
@@ -28,13 +28,6 @@ def adj_to_bias(adj, sizes, nhood=1):
     return -1e9 * (1.0 - mt)
 
 def get_neighbours(G,u,in_out='in'):
-    """
-    Output in degree neighbor node and out degree neighbor node
-    :param G:
-    :param u:
-    :param in_out:
-    :return:
-    """
     neighbours = set()
     if in_out == 'in':
         edges = set(G.in_edges(u))
@@ -48,43 +41,3 @@ def get_neighbours(G,u,in_out='in'):
             neighbours.add(edge[1])
     return neighbours
 
-# File Related
-def read_node_label(filename, skip_head=False):
-    fin = open(filename, 'r')
-    X = []
-    Y = []
-    while 1:
-        if skip_head:
-            fin.readline()
-        l = fin.readline()
-        if l == '':
-            break
-        vec = l.strip().split(' ')
-        X.append(vec[0])
-        Y.append(vec[1])
-        print(vec[0])
-    fin.close()
-    return X, Y
-
-def save_node_label(filename, x,y,z):
-    f_out = open(filename, 'w+')
-    i = 0
-    for idx in x:
-        t=''
-        t = str(x[i]) + ' '+str(y[i]) + ' '+str(z[i])
-        f_out.write(t)
-        f_out.write('\n')
-        i = i + 1
-    f_out.close()
-    return x,y,z
-
-def save_rank(filename, x):
-    f_out = open(filename, 'w+')
-    i = 0
-    for idx in x:
-        t=''
-        t = str(x[i])
-        f_out.write(t)
-        f_out.write('\n')
-        i = i + 1
-    f_out.close()
